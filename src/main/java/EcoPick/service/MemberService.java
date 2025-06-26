cb4e86eeeaaa61a25bdd627ed587afff947d9607
halee <sleepoem43@gmail.com>
2025-06-27 00:44:13 +0900

    feat: member 부가정보 조회 기능 추가

--- src/main/java/EcoPick/service/MemberService.java
+++ src/main/java/EcoPick/service/MemberService.java
@@ -1,7 +1,12 @@
 package EcoPick.service;
 
+import EcoPick.domain.company.Company;
+import EcoPick.domain.coupon.Coupon;
 import EcoPick.domain.member.Member;
+import EcoPick.repository.CompanyRepository;
 import EcoPick.repository.MemberRepository;
+import EcoPick.repository.coupon.CouponJpaRepository;
+import EcoPick.repository.coupon.MemberCouponRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +15,12 @@ import java.util.List;
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final MemberCouponRepository memberCouponRepository;
 
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository, MemberCouponRepository memberCouponRepository) {
+
         this.memberRepository = memberRepository;
+        this.memberCouponRepository = memberCouponRepository;
     }
 
     // member 생성
@@ -35,5 +43,22 @@ public class MemberService {
         memberRepository.deleteById(id);
     }
 
+    // count 조회
+    public int getUserCount(Long id){
+        Member member = memberRepository.findById(id).get();
+        return member.getCount();
+    }
+
+    // point 조회
+    public int getUserPoint(Long id){
+        Member member = memberRepository.findById(id).get();
+        return member.getPoint();
+    }
+
+    // 멤버가 소유한 coupon list 조회
+    public List<Coupon> getUserCoupon(Long id){
+        Member member = memberRepository.findById(id).get();
+        return memberCouponRepository.findCouponsByMemberId(member.getId());
+    }
 
 }
