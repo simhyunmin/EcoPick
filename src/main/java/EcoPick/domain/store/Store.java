9ce043a47593f62fa034fa7c42d9b4a1e8fb25da
halee <sleepoem43@gmail.com>
2025-06-27 00:41:36 +0900

    feat: store 조회 로직 추가

--- src/main/java/EcoPick/domain/store/Store.java
+++ src/main/java/EcoPick/domain/store/Store.java
@@ -1,19 +1,14 @@
-package EcoPick.domain.company.store;
+package EcoPick.domain.store;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+
+@Entity
 public class Store {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String name;
-
     private String address;
     private String icon_src;
-
-
-
 }
--- src/main/java/EcoPick/repository/StoreRepository.java
+++ src/main/java/EcoPick/repository/StoreRepository.java
@@ -1,6 +1,6 @@
 package EcoPick.repository;
 
-import EcoPick.domain.company.store.Store;
+import EcoPick.domain.store.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
