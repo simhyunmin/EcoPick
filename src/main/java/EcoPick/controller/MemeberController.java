1d4464716068eaca6c9cbf35606a86768f78e6e5
halee <sleepoem43@gmail.com>
2025-06-26 16:58:58 +0900

    feat: service controller 구조 추가

---
+++ src/main/java/EcoPick/controller/MemeberController.java
@@ -0,0 +1,16 @@
+package EcoPick.controller;
+
+
+import EcoPick.service.MemberService;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MemeberController {
+
+    public MemeberController(MemberService memberService) {
+
+
+    }
+}
---
+++ src/main/java/EcoPick/service/MemberService.java
@@ -0,0 +1,14 @@
+package EcoPick.service;
+
+
+import EcoPick.controller.MemeberController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+public class MemberService {
+
+}
