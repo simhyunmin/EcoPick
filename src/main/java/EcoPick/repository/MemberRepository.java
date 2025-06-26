5f70095e138a72e333a8cce53f4db578f20ef9a0
halee <sleepoem43@gmail.com>
2025-06-26 17:15:45 +0900

    feat: MemberRepository 추가

---
+++ src/main/java/EcoPick/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package EcoPick.repository;
+
+import EcoPick.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
