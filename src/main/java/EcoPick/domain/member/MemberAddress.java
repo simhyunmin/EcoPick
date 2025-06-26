d87d4ba3100988a78a372ed084dbf4f15198fe0e
simhyunmin <shm66371945@gmail.com>
2025-06-26 16:10:07 +0900

    feat : MemberAddress 수정

--- src/main/java/EcoPick/domain/member/MemberAddress.java
+++ src/main/java/EcoPick/domain/member/MemberAddress.java
@@ -1,4 +1,21 @@
 package EcoPick.domain.member;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String address;
+    private String detail_address;
+    private double position_x;
+    private double position_y;
+
+
 }
