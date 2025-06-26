9fdfb75059cd76ac67880715de34c73b64403f0a
simhyunmin <shm66371945@gmail.com>
2025-06-26 10:43:28 +0900

    기본 구조 생성

---
+++ backend/src/main/java/EcoPick/EcoPickApplication.java
@@ -0,0 +1,13 @@
+package EcoPick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoPickApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/EcoPick/EcoPick/EcoPickApplicationTests.java
@@ -0,0 +1,13 @@
+package EcoPick.EcoPick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
